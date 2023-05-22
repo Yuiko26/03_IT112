0b4db14b091686d73e12dfe9416eb027ab8719ae
Yuiko26 <yuiko.koike10@gmail.com>
2023-05-21 19:23:54 -0700

    Unit6_complete

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Book, Review
 from .utils import average_rating
-from .forms import ExampleForm, SearchForm
 
 def book_list(request):
     books = Book.objects.all()
